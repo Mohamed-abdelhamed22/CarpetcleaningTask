ecfc9a740800ad9cec14f58878e4a8d20eea68b9
Mohamed-abdelhamed22 <mohamed.abdelhamed6112k@gmail.com>
2025-10-02 21:43:37 +0300

    Add project files.

---
+++ ConsoleApp1/task1.cs
@@ -0,0 +1,19 @@
+﻿double SmallPrice = 25;
+double LargePrice = 35;
+double TaxRate = 0.06;
+string ValidationMessage = "This estimate valid for 30 days";
+double SmallCount = double.Parse(Console.ReadLine());
+double LargeCount = double.Parse(Console.ReadLine());
+
+System.Console.WriteLine($"Number of Small Carpets: {SmallCount}");
+System.Console.WriteLine($"Number of Large Carpets: {LargeCount}");
+System.Console.WriteLine($"Price per Small Carpet: {SmallPrice}");
+System.Console.WriteLine($"Price per Large Carpet: {LargePrice}");
+double Cost = (SmallPrice * SmallCount) + (LargePrice * LargeCount);
+System.Console.WriteLine($"cost: {Cost}");
+double Tax = Cost * TaxRate;
+System.Console.WriteLine($"Tax: {Tax}");
+double Total = Cost + Tax;
+Console.WriteLine("================================");
+System.Console.WriteLine($"Total estimate: {Total}");
+System.Console.WriteLine(ValidationMessage);
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+arr[0] = 100;
+int[] arr= [0, 0, 0, 0, 0];
+arr[arr.Length - 1] = 1000;
+//arr[4] = 1000;
+//arr[^1] = 1000; 
+System.Console.WriteLine(arr[0]);
+System.Console.WriteLine(arr[arr.Length - 1]);
